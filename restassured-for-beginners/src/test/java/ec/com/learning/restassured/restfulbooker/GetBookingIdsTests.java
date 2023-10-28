9ae0d4429a8eebf5b419d5a1c390e5aa3743af6a
Steven <stevengf14@gmail.com>
2023-10-27 20:23:01 -0500

    query parameters

--- restassured-for-beginners/src/test/java/ec/com/learning/restassured/restfulbooker/GetBookingIdsTests.java
+++ restassured-for-beginners/src/test/java/ec/com/learning/restassured/restfulbooker/GetBookingIdsTests.java
@@ -23,4 +23,22 @@ public class GetBookingIdsTests extends BaseTest {
 		List<Integer> bookingIds = response.jsonPath().getList("bookingId");
 		Assert.assertFalse(bookingIds.isEmpty(), "List of bookingIds is empty, but it shouldn't be");
 	}
+
+	@Test
+	public void getBookingIdsWithFilterTest() {
+		// Add query parameter to spec
+		spec.queryParam("firstname", "Jim");
+		spec.queryParam("lastname", "Smith");
+
+		// Get response with booking ids
+		Response response = RestAssured.given(spec).get("/booking");
+		response.print();
+
+		// Verify response 200
+		Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
+
+		// Verify at least 1 booking id in response
+		List<Integer> bookingIds = response.jsonPath().getList("bookingId");
+		Assert.assertFalse(bookingIds.isEmpty(), "List of bookingIds is empty, but it shouldn't be");
+	}
 }
